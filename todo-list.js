651d76361c84b45ded5355281187aaec10401198
wata00913 <175d8639@gmail.com>
2022-12-06 15:11:19 +0900

    todoListデータを永続化する処理を追加

--- todo-list.js
+++ todo-list.js
@@ -5,9 +5,12 @@ export default {
     ToDo,
   },
   setup() {
-    const newContent = ref("");
-    const todoList = ref([{ content: "テストです" }, { content: "ToDo2" }]);
     const message = ref("");
+
+    const rawDataStr = window.localStorage.getItem("todoList");
+    const todoList = ref(rawDataStr ? JSON.parse(rawDataStr) : []);
+
+    const newContent = ref("");
 
     function onCreateToDo() {
       if (!newContent.value) {
@@ -15,14 +18,21 @@ export default {
         return;
       }
       todoList.value.push({ content: newContent });
+      save(todoList.value);
     }
 
     function edit(content, idx) {
       todoList.value.splice(idx, 1, { content: content });
+      save(todoList.value);
     }
 
     function destroy(idx) {
       todoList.value.splice(idx, 1);
+      save(todoList.value);
+    }
+
+    function save(todoList) {
+      window.localStorage.setItem("todoList", JSON.stringify(todoList));
     }
     return {
       newContent,
